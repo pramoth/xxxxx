b51a69c02415491f8e5cb5b7618c28dce1942919
Pramoth <pramoth@geniustree.co.th>
2023-05-02 16:38:47 +0700

    first commit

---
+++ src/main/java/th/co/geniustree/intern/chatgpt/App.java
@@ -0,0 +1,10 @@
+package th.co.geniustree.intern.chatgpt;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/th/co/geniustree/intern/chatgpt/AppTest.java
@@ -0,0 +1,20 @@
+package th.co.geniustree.intern.chatgpt;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
